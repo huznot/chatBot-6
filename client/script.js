76d4fed73601cd8b00838d028d3c24e0ad21b9d7
huznot <huznot@gmail.com>
2023-03-18 18:44:45 -0500

    update the server url

--- client/script.js
+++ client/script.js
@@ -98,7 +98,7 @@ const handleSubmit = async (e) => {
 
   // fetch bot response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatai-ii0q.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
